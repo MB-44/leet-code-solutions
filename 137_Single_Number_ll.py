7e4da3f697e91d19c3dc8b2c38de48cc6d4163d1
Menath <93506278+Lakvindubaddegama@users.noreply.github.com>
2023-11-14 21:20:13 +0530

    second

--- 137_Single_Number_ll.py
+++ 137_Single_Number_ll.py
@@ -2,9 +2,14 @@
 # Medium 
 # Arrays | Bit manipulation
 
-
-
 def singleNumber(nums):
     uniqueNums = list(set(nums))
-    for num in uniqueNumsNums:
-        if 
+    for num in uniqueNums:
+        if nums.count(num) == 1:
+            return num
+        
+def singleNumber(nums):
+    for i in range(nums):
+        times = nums.count(nums[i])
+        if times == 1:
+            return nums[i]
