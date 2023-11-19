6ebd6a56c57d3a91c0c9be9032cbce43ebe1959e
Menath <93506278+Lakvindubaddegama@users.noreply.github.com>
2023-11-19 10:32:48 +0530

    18ms | 11/19 | Daily

---
+++ 1980. Find unique binary string.py
@@ -0,0 +1,9 @@
+# 18ms
+
+def findDifferentBinaryString(nums):
+    n = len(nums[0])
+    for i in range(2**n):
+        binary = bin(i)[2:].zfill(n)
+        if binary not in nums:
+            return binary
+
