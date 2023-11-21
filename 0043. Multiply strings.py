4b9b086c8f063c95840684da8e4cdd7763238ee7
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-21 18:03:29 +0530

    16ms | 11/21

--- 0043. Multiply strings.py
+++ 0043. Multiply strings.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def multiply(self,num1,num2):
-        pass
-
-
-
+        def func(stringInt):
+            num = 0
+            for ch in stringInt:
+                if "0" <= ch <= "9":
+                    num = num * 10 +(ord(ch) - ord("0"))
+            return num
+        return str(func(num1) * func(num2))
 
 if __name__ == "__main__":
     num1,num2 = "2","3"
