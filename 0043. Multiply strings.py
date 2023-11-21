9a812e7271b2d986e3568df0af871cd923418fa5
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-21 17:41:14 +0530

    create | 11/21

---
+++ 0043. Multiply strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self,num1,num2):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    num1,num2 = "2","3"
+    x = Solution().multiply(num1,num2)
+    print(x)
