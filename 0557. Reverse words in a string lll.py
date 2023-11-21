ba5d78a43265776fc7b53919abcd58ee83377cd4
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-21 08:36:39 +0530

    21ms | 11/20

--- 0557. Reverse words in a string lll.py
+++ 0557. Reverse words in a string lll.py
@@ -5,11 +5,9 @@ class Solution(object):
             reversedList.append(word[::-1])
         return " ".join(reversedList)
 
-
 # class Solution(object):
 #     def reverseWords(self, s):
-#         return " ".join([i[::-1] for i in s.split()])
-        
+#         return " ".join([i[::-1] for i in s.split()])    
 
 if __name__ == "__main__":
     s = "Let's take Leetcode contest"
