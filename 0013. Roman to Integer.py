4c65166a2647846840ee245cc7e2006869b544b9
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-22 20:28:31 +0530

    36ms | 11/22

---
+++ 0013. Roman to Integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str):
+        dict = {
+            "M": 1000,
+            "D": 500,
+            "C": 100,
+            "L": 50,
+            "X": 10,
+            "V": 5,
+            "I": 1 }
+
+        temp, total = 0, 0
+        romanBackList = list(s)[::-1]
+        for c in romanBackList:
+            if temp == 0:
+                total += dict[c]
+            elif temp > dict[c]:
+                total -= dict[c]
+            else:
+                total += dict[c]
+            temp = dict[c]
+        return total
+
+
