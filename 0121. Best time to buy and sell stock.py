8c53a8fbcec7b38c011821f3a689df6bb531c1bd
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-22 20:41:20 +0530

    2948ms | 11/22

---
+++ 0121. Best time to buy and sell stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]):
+            maxProfit,minProfit = 0,prices[0]
+            for index in range(1,len(prices)):
+                if prices[index] < minProfit:
+                    minProfit=prices[index]
+                if maxProfit < prices[index]-minProfit:
+                    maxProfit = prices[index]-minProfit
+            return maxProfit
+
+# prices is linked list, 
+# if you want to run this in vscode you hv to create a linkedlist called prices
