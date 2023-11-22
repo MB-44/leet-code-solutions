bfdfb7becd54be6b42e30d4ba2b07e20c95d7105
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-22 18:20:50 +0530

    11/22

--- test_solutions.py
+++ test_solutions.py
@@ -1,7 +1,10 @@
+# my_string = "123"
+# num = 0
+# for ch in my_string:
+#     if "0" <= ch <= "9":
+#         num = num * 10 +(ord(ch) - ord("0"))
+# print(type(num))
 
-my_string = "123"
-num = 0
-for ch in my_string:
-    if "0" <= ch <= "9":
-        num = num * 10 +(ord(ch) - ord("0"))
-print(type(num))
+for i in range(5):
+    for j in range(i,5):
+        print(i,j)
