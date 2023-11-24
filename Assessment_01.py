1b364f7de9c5c16148bcc6c3149c5cfe7b87a774
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-24 18:36:34 +0530

    100p

---
+++ Assessment_01.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseVowels(self,s):
+        vowels = "aeiou"
+        s = list(s)
+        vowelsInS = ""
+        for letter in s:
+            if letter.lower() in vowels:
+                vowelsInS += letter
+        count = 0
+        vowelsInS = vowelsInS[::-1]
+        for i in range(len(s)):
+            if s[i].lower() in vowels:
+                s[i] = vowelsInS[count]
+                count += 1
+        return "".join(s)
+
+if __name__ == "__main__":
+    s = "hello"
+    result = Solution().reverseVowels(s)
+    print(result)
+
+# hello = "men"
+# print(list(hello))
