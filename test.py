016a196acdf838ee00b4189287f71ec3b41b7739
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-27 21:05:17 +0530

    --

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-matrix = [[1,1,1],[1,0,1],[1,1,1]]
-
-indexs = [] 
-for list in matrix:
-    if 0 in list:
-        indexs.append([matrix.index(list),list.index(0)])
-
-for i in range(len(matrix)):
-    if i in [row[0] for row in indexs]:
-        matrix[i] = [0]*len(matrix[i])
-        continue
-    for j in range(len(matrix[i])):
-        if j in [row[j] for row in indexs]:
-            for k in range(len(matrix)):
-                if len(matrix[i]) > j:
-                    matrix[k][j] = [0]*len(matrix)
+s = "bbb"
+x = len(set(s))
+print(x)
