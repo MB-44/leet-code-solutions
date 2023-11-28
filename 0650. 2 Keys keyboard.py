d6aa7c8a38310118b1f499d30c90930d83a93e32
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-28 21:51:01 +0530

    to do

---
+++ 0650. 2 Keys keyboard.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minSteps(self,n: int)->int:
+        if n == "A":
+            return 0
+        
+
+
+if __name__ == "__main__":
+    n = 3
+    result = Solution().minSteps(n)
+    print(result)
