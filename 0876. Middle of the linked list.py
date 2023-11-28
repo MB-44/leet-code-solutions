e44be7cb1577c9ac3bb64848dbf6a409aa6f7e90
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-28 22:00:40 +0530

    39ms | 11/28 | 10.00pm

---
+++ 0876. Middle of the linked list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current, length = head, 0
+        while current is not None:
+            length += 1
+            current = current.next
+        current = head
+        midId = (length//2)+1
+        while midId > 1:
+            current = current.next
+            midId -= 1
+        return current
+
+
+
+
+        
