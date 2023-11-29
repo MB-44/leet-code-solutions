bffd317bb03761bb763aad53d94aaeb117a339f1
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-29 11:48:01 +0530

    to do

--- 0171. Excel sheet column number.py
+++ 0171. Excel sheet column number.py
@@ -1,15 +1,16 @@
 class Solution:
     def titleToNumber(self,columnTitle:str)->int:
         if len(columnTitle) == 1:
-            return ord(columnTitle)%64
-        titleNum,count,i = 0,0,len(columnTitle)
+            return ord(columnTitle) - ord('A')+1
+        
+        titleNum,count = 0,len(columnTitle)
+        
         for letter in columnTitle[::-1]:
-            titleNum += ord(letter)%64
-            count = 26
-            i-=1
-            if i!=0:
-                titleNum+=count
+            titleNum = titleNum +  ((26**count-1) * ord(letter)%64)
+            count -= 1
+        
         return titleNum
+
 
 if __name__ == "__main__":
     title = "ZY"
