33097df8912edf18d1d6833da03fa6b13c591cf7
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-29 08:50:47 +0530

    --

---
+++ 0203. Remove linked list elements.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], value: int) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        while head and head.val == value:
+            head = head.next
+
+        current = head
+        while current and current.next:
+            if current.next.val == value:
+                current.next = current.next.next
+                continue
+            current = current.next
+        return current
--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-str = "101"
-dec,x = 0,0
-for i in str[::-1]:
-    dec += (2**x)*int(i)
-    x += 1
-print(dec)
+s = "101"
+print(int(s,2))
