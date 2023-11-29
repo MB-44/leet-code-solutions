2b2b574d377660604019e9cc9ed1fda3288d5d0b
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-29 11:24:31 +0530

    37ms | 11/29

--- 0067. Add Binary.py
+++ 0067. Add Binary.py
@@ -3,7 +3,7 @@ class Solution(object):
         addedBin = bin(int(a,2) + int(b,2))
         return str(addedBin[2:])
 
-
+#
 if __name__ == "__main__":
     a, b = "11","1"
     x = Solution().addBinary(a,b)
