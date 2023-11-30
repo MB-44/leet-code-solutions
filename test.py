9ca0b5fe5f9b4c4602e018f81e975a17fa44c6a2
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-30 22:20:11 +0530

    --

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-class Solution:
-    def dayOfTheWeek(day,month,year):
-        daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        dateObj = datetime.date(year,month,day)
-        dayOfWeek = dateObj.weekday(dateObj)
-        return daysOfWeek[dayOfWeek]
