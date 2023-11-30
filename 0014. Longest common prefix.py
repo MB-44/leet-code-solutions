a354af0f0d87a9fa642e72fe2bc4a1402bffb9df
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-30 09:46:23 +0530

    trying to do

--- 0014. Longest common prefix.py
+++ 0014. Longest common prefix.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def longestCommonPrefix(self,strs:list[str])->str:
-        for i in strs:
-            for j in i:
-                if j in [k for k in strs]
+        for i in range(len(strs[0])):
+            if str[0][i] in [j for j in str[1:]]
+
+
 
 
 if __name__ == "__main__":
     strs = ["flower","flight","flow"]
-    print(set(strs[0]))
+    
