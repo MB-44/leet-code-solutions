9181a2243a64b00d481bbc79c7ba9345134c7e45
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-02 20:44:48 +0530

    44ms

--- 1551. Minimum operations to make array equal.py
+++ 1551. Minimum operations to make array equal.py
@@ -1,3 +1,13 @@
 class Solution:
     def minOperations(n: int) -> int:
         return (n*n) // 4
+
+# [1,3,5,7,9,11]
+# [1,3,5,7,9]
+# n-1
+
+# [1,3,5]
+# [2,4,8]
+# n-1
+
+# s = (first_element + last element (n-1)) / (n // 2)
