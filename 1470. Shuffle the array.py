018564aabfa2b8fd66003af7e09d5097721f484d
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-04 22:55:15 +0530

    59ms | 12/04

---
+++ 1470. Shuffle the array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shuffle(self, nums: list[int], n: int) -> list[int]:
+        part1, part2 = nums[:n], nums[n:]
+        output = []
+        for p1, p2 in zip(part1,part2):
+            output.append(p1,p2)
