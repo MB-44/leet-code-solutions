7fa578e2b97b6907b18b3ac021fbe840103b8187
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-07 22:30:52 +0530

    going forward

--- 0008. String to integer (atoi).py
+++ 0008. String to integer (atoi).py
@@ -1,15 +1,18 @@
 class Solution(object):
     def myAtoi(self,s):
         s = s.strip()
-        if not s or (len(s)==1 or not(s.isdigit())):
+        if not s or (len(s)==1 and not(s[:].isdigit())):
             return 0
         integer, op = "", 1
         for i in s:
-            if i in "-" and s[s.index(i)+1].isdigit():
+            if i == "-" and s[s.index(i)+1].isdigit():
                 op = -1
                 continue
             if i.isdigit():
                 integer += i
+            elif i == "+":
+                op = 1
+                continue
             else: 
                 break
         if not integer:
@@ -20,5 +23,6 @@ class Solution(object):
         return max(min(integer,2**31-1),-2**31)
 
 if __name__ == "__main__":
-    s = "-"
+    s = "42"
     result = Solution().myAtoi(s)
+    print(result)
