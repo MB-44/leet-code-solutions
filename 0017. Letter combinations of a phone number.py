dff5bcee6b4a776232e523c38c3b4d7b94205530
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-08 22:19:35 +0530

    --

--- 0017. Letter combinations of a phone number.py
+++ 0017. Letter combinations of a phone number.py
@@ -1,8 +1,16 @@
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
+        
         dic = {
             "2":"abc",  "3":"def",  "4":"ghi",
             "5":"jkl",  "6":"mno",  "7":"pqrs",
             "8":"tuv",  "9":"wxyz"  }
         
         lis = []
+        
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                lis.append(curStr)
+                return
+        
+        
