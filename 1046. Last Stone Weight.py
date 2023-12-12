3f198ce43ec15e453bcef7ddece95dc0e8c4ffa1
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-12 18:38:37 +0530

    to do | 12/12

---
+++ 1046. Last Stone Weight.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        if len(stones) == 1:
+            return stones[0]
+        
+        while len(stones) > 1:
+            max01 = max(stones)
+            stones.remove(max01)
+            max02 = max(stones)
+            newStone = max01 - max02
+            stones.append*newStone
+
+
+
+
+if __name__ == "__main__":
+    pass
+        
