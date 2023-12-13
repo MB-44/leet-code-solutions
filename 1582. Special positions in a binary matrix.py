f1dfe8f8ea60beedade14e9600bbd9c1c83bf8d5
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-14 00:42:01 +0530

    147ms | 12/13

--- 1582. Special positions in a binary matrix.py
+++ 1582. Special positions in a binary matrix.py
@@ -1,3 +1,4 @@
+# not this one
 class Solution:
     def numSpecial(mat: list[list[int]]) -> int:
         indexs = []
@@ -11,13 +12,46 @@ class Solution:
                 indexs.append([row,itemCount])
             itemCount += 1
         
-        for index in indexs:
-            if indexs:
-                pass
+        rows = []
+        cols = []
 
+        for item in indexs:
+            rows.append(item[0]), cols.append(item[1])
 
-if __name__ == "__main__":
-    mat = [[1,0,0],[0,0,1],[1,0,0]]
-    result = Solution.numSpecial(mat)
-    print(result)
+        uniqueRows = list(set(rows))
+        uniqueCols = list(set(cols))
+
+        mightBe01 = []
+        for i,j in zip(uniqueRows,uniqueCols):
+            if rows.count(i) == 1 and cols.count(j) == 1:
+                mightBe01.append([i,j])
         
+        count = 0
+        for item in mightBe01:
+            if mat[item[0]][item[1]] == 1:
+                count += 1
+        return count
+
+
+class Solution:
+    def numSpecial(self, nums: list[list[int]]) -> int:
+        result = 0
+        columns = [0] * len(nums[0])
+
+        for i in range(len(nums)):
+            ones_in_the_row = 0
+            column = 0
+
+            for j in range(len(nums[i])):
+                if nums[i][j] == 1:
+                    ones_in_the_row += 1
+
+                    if ones_in_the_row == 1:
+                        columns[j] = 1 if columns[j] == 0 else 2
+                        column = j
+                    else:
+                        columns[column] = 2
+                        columns[j] = 2
+
+        result += sum(column == 1 for column in columns)
+        return result        
