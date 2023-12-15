7b0196087877e5c6c99c383da21530726a2d6ae7
1 <menathlakvindub@gmail.com>
2023-12-15 08:27:03 +0530

    53ms | 12/15

---
+++ 1436. Destination city.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+
+    const cities = new Set();
+    
+    for (const path of paths) {
+        cities.add(path[0]);
+    }
+
+    for (const path of paths) {
+        const destination = path[1];
+        if (!cities.has(destination)) {
+            return destination;
+        }
+    }
+};
