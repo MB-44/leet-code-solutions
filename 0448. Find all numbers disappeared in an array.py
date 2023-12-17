8bc9e88bb9482b0320e4f340b48b336530b5a3d5
1 <menathlakvindub@gmail.com>
2023-12-17 18:03:06 +0530

    304ms | 12/17

--- 0448. Find all numbers disappeared in an array.py
+++ 0448. Find all numbers disappeared in an array.py
@@ -1,7 +1,16 @@
 class Solution:
     def findDisappearedNumbers(nums):
-        disaArray = [i for i in range(1,len(nums+1)) if i not in nums]
-        return disaArray
+        data = {}
+        for d in nums:
+            data[d] = True
+        
+        disaArr = []
+        for i in range(1, len(nums) + 1):
+            if i not in data.keys():
+                disaArr.append(i)
+        return disaArr
 
-result = Solution.findDisappearedNumbers([4,3,2,7,8,2,3,1])
-print(result)
+
+if __name__ == "__main__":
+    result = Solution.findDisappearedNumbers([4,3,2,7,8,2,3,1])
+    print(result)
