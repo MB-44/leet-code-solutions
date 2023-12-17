bd913bbed07ab6baa23095655aef7db85a7d6559
1 <menathlakvindub@gmail.com>
2023-12-17 07:50:15 +0530

    40ms | 12/17

--- 0029. Divide two integers.py
+++ 0029. Divide two integers.py
@@ -1,16 +1,28 @@
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
-        if (dividend > 0) == (divisor > 0):
-            sign = 1
-        else: 
-            sign = -1
+# class Solution:
+#     def divide(self, dividend: int, divisor: int) -> int:
+#         if (dividend > 0) == (divisor > 0):
+#             sign = 1
+#         else: 
+#             sign = -1
         
-        quotient = 0
-        dividend = abs(dividend)
-        divisor = abs(divisor)
+#         quotient = 0
+#         dividend = abs(dividend)
+#         divisor = abs(divisor)
 
-        while dividend >= divisor:
-            dividend -= divisor
-            quotient += 1
+#         while dividend >= divisor:
+#             dividend -= divisor
+#             quotient += 1
         
-        return max(min(quotient*sign,2**31-1),-2**31-1)
+#         return max(min(quotient*sign,2**31-1),-2**31-1)
+
+class Solution:
+    def divide(dividend:int, divisor: int) -> int:
+        if ((dividend < 0) and (divisor < 0)) or ((dividend > 0) and (divisor > 0)): 
+            sign = 1
+        else:
+            sign = -1
+
+
+
+if __name__ == "__main__":
+    pass
--- test_solutions.py
+++
@@ -1,3 +0,0 @@
-mystr = [1,2,4,4]
-mystr.remove(4)
-print(mystr)
