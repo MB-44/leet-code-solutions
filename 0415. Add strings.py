bc99facc5f3a7f9879f1ab9d21763c35989fc034
1 <menathlakvindub@gmail.com>
2023-12-17 18:04:04 +0530

    44ms | 12/17

--- 0415. Add strings.py
+++ 0415. Add strings.py
@@ -1,6 +1,6 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        sys.set_int_max_str_digits(10000)
+        # sys.set_int_max_str_digits(10000)
         num1 = int(num1)
         num2 = int(num2)
         return str(num1 + num2)
