c18a8c932f43782f0d084fa266e11a442d9dadad
1 <menathlakvindub@gmail.com>
2023-12-20 08:19:42 +0530

    59ms | 12/20

---
+++ 2706. Buy two chocalates.py
@@ -0,0 +1,15 @@
+class Solution:
+    def buyChoco(prices, money):
+        toBuy = prices[0] + prices[1]
+        if toBuy > money:
+            return money
+        return (money - toBuy) 
+    
+
+if __name__ == "__main__":
+    prices = [3,1,2,2]
+    money = 3
+    result = Solution.buyChoco(prices, money)
+    print(result)
+        
+        
