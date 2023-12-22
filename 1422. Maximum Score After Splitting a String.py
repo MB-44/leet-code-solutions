16e8b10aa5ce01d6027a5619714ab230b42d5df0
1 <menathlakvindub@gmail.com>
2023-12-22 08:37:35 +0530

    fix line endings

---
+++ 1422. Maximum Score After Splitting a String.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        maxScore = 0
+        for i in range(1,len(s)):
+            leftSubCount = s[:i].count("0")
+            rightSubCount = s[i:].count("1")
+            maxScore = max(maxScore, (rightSubCount + leftSubCount))
+        return maxScore
+
+# s = "011101"
+# Solution.maxScore(s)
+# result = Solution.maxScore(s)
+# print(result)
