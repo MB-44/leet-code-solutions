3c5f4495d72ee65e33b725cf696f390fdb241360
1 <menathlakvindub@gmail.com>
2023-12-23 20:19:50 +0530

    unhappy bruh

---
+++ 100171. Count the number of incremovable subarrays l.py
@@ -0,0 +1,13 @@
+class Solution:
+    def incremovableSubarrayCount(self, nums: list[int]) -> int:
+        n = len(nums)
+        result = 0
+        end = 0
+        
+        for start in range(n):
+            while end < n and (end == start or nums[end] > nums[end - 1]):
+                end += 1
+            
+            result += (end - start)
+        
+        return result
