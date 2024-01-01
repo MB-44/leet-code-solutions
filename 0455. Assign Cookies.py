c57ddef599fc8acee613de4d5e040869094163c3
1 <menathlakvindub@gmail.com>
2024-01-01 08:59:51 +0530

    123ms | 01/01

--- 0455. Assign Cookies.py
+++ 0455. Assign Cookies.py
@@ -11,6 +11,25 @@
 #                 continue
 #         return count
             
+
+class Solution:
+    def findContentChildren(g: list[int], s: list[int]) -> int:
+        ans = 0
+        i, j = 0, 0
+        
+        s.sort(reverse=True)
+        g.sort(reverse=True)
+
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                ans += 1
+                i += 1
+                j += 1
+            else:
+                i += 1
+        
+        return ans
+
 if __name__ == "__main__":
     g = [10, 9, 8, 7]
     s = [5, 6, 7, 8]
