f004e276440814ae7576806e885c2a30506ca866
1 <menathlakvindub@gmail.com>
2024-01-03 21:19:06 +0530

    77ms | 01/03

--- 2828. Check if a string is an Acronym of words.js
+++ 2828. Check if a string is an Acronym of words.js
@@ -1,9 +1,14 @@
-const acronymWords = (words, s) => {
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function(words, s) {
     let firstLetters = "";
-    
+
     for (let i = 0; i < words.length; i++) {
         firstLetters += words[i][0];
     }
 
     return firstLetters == s;
-}
+};
